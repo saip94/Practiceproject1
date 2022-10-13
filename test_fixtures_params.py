ca2b7d89e2f225e335e701b1e230d1921be984d9
saip94 <74102713+saip94@users.noreply.github.com>
2022-10-13 18:19:34 +0530

    Update test_fixtures_params.py

--- test_fixtures_params.py
+++ test_fixtures_params.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(params=['chrome','firefox'],scope='class')
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = webdriver.Chrome(ChromeDriverManager().install())
+        web_driver = webdriver.Chrome()
     if request.param == "firefox":
         firefox_path="E:/Chrome downloads/geckodriver-v0.31.0-win64/geckodriver.exe"
         web_driver = webdriver.Firefox(executable_path=firefox_path)
