26002b0a359215a3f49c6389a34890a1901db3f7
saip94 <74102713+saip94@users.noreply.github.com>
2022-10-13 18:18:11 +0530

    Update test_firsttest.py

--- test_firsttest.py
+++ test_firsttest.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 def test_google():
-    driver=webdriver.Chrome(ChromeDriverManager().install())
+    driver=webdriver.Chrome()
     driver.implicitly_wait(10)
     driver.get("https://www.google.com")
     assert driver.title=='Google','title failed'
